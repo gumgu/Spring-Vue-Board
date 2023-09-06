3acb7047f2cdfd23d2f39c40efa7fbb52dc6dc74
gumgu <bienlee@naver.com>
2023-09-06 15:48:37 +0900

    first upload

---
+++ BoardManagement/src/main/java/com/study/admin/exception/WebConfig.java
@@ -0,0 +1,38 @@
+package com.study.admin.exception;
+
+import com.study.admin.exception.filter.LogFilter;
+import com.study.admin.exception.interceptor.LogInterceptor;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.naming.ldap.BasicControl;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**"); // 오류 페이지 경로
+    }
+
+
+//    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        // 이 필터는 request, error 두가지의 경우에 호출된다.
+        return filterRegistrationBean;
+    }
+}
